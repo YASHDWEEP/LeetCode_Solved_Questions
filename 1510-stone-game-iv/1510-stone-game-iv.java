5dd9aac465ff19a8de5c9a8e5a2747322e8d09af
YASHDWEEP <rewatkhedkar@gmail.com>
2026-08-10 10:54:07 +0530

    Time: 14 ms (91.38%), Space: 42.3 MB (90.34%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -3,17 +3,14 @@ class Solution {
 
         boolean[] dp = new boolean[n + 1];
 
-        // dp[0] = false
-        // No stones -> current player loses
+       
 
         for (int i = 1; i <= n; i++) {
 
-            // Try every perfect square
             for (int j = 1; j * j <= i; j++) {
 
                 int square = j * j;
 
-                // If we can make opponent reach a losing state
                 if (!dp[i - square]) {
                     dp[i] = true;
                     break;

31daec2e32b8567903be8bd8d5269ce1c180cd85
YASHDWEEP <rewatkhedkar@gmail.com>
2026-08-17 14:42:26 +0530

    Time: 3 ms (100%), Space: 114.8 MB (52.32%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -1,22 +1,14 @@
 class Solution {
-
     public boolean stoneGameIX(int[] stones) {
-        int cnt0 = 0,
-            cnt1 = 0,
-            cnt2 = 0;
-        for (int val : stones) {
-            int type = val % 3;
-            if (type == 0) {
-                ++cnt0;
-            } else if (type == 1) {
-                ++cnt1;
-            } else {
-                ++cnt2;
-            }
+        int[] cnt = new int[3];
+        for (int s : stones) cnt[s % 3]++;
+
+        if (cnt[1] == 0 && cnt[2] == 0) return false;
+
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
+        } else {
+            return Math.abs(cnt[1] - cnt[2]) > 2;
         }
-        if (cnt0 % 2 == 0) {
-            return cnt1 >= 1 && cnt2 >= 1;
-        }
-        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
     }
 }

1a1f2d5121abc8aa4e77382d4b89c24416ccfaf5
YASHDWEEP <rewatkhedkar@gmail.com>
2026-08-23 15:00:21 +0530

    Time: 7 ms (79.56%), Space: 47.2 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+       for (int i = 0; i < n / 2; i++) {
+
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                leftQ++;
+            } else {
+                leftSum += ch - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                rightQ++;
+            } else {
+                rightSum += ch - '0';
+            }
+        }
+
+        if (2 * (leftSum - rightSum) == 9 * (rightQ - leftQ)) {
+            return false;
+        }
+
+        return true;
+    }
+}

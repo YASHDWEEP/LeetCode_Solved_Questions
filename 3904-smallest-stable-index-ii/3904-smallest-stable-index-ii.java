2ab71d018a6fa81eee9fdc2864308ce5b169047a
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-05 20:29:45 +0530

    Time: 3 ms (100%), Space: 133.3 MB (20.99%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,18 +1,24 @@
-class Solution {
-    public int firstStableIndex(int[] nums, int k) {
-       int n=nums.length;
-        int[] right=new int[n];
-        right[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-         right[i]=Math.min(right[i+1],nums[i]);
+class Solution 
+{
+    public int firstStableIndex(int[] nums, int k) 
+    {
+        int n = nums.length;
+        int[] mini = new int[n];
+
+        int mint = Integer.MAX_VALUE;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if (nums[i] < mint) mint = nums[i];
+            mini[i] = mint;
         }
-        int left=0;
-        for(int i=0;i<n;i++){
-            left=Math.max(left,nums[i]);
-            if(left-right[i]<=k){
-                return i;
-            }
+
+        int maxt = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > maxt) maxt = nums[i];
+            if(maxt - mini[i] <= k) return i;
         }
+
         return -1;
     }
 }

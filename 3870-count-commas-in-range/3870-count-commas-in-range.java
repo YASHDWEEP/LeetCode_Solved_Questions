50273f0d010e8dfa8000095a3c6974802c34e942
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-08 14:50:34 +0530

    Time: 0 ms (100%), Space: 42.8 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,25 +1,12 @@
 class Solution {
-    public int lengthofn(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n / 10;
-            count += 1;
-        }
-        return count;
-    }
+  
 
     public int countCommas(int n) {
-        int count_commas = 1 ; 
-        if (lengthofn(n) < 4){
+        if (n < 999){
             return 0 ; 
         }
-        // if (n == 100000){
-        //     count_commas+=2 ; 
-        // }
         
-            int total_numbers = n - 1000; 
-            count_commas += total_numbers;
-        
-        return count_commas; 
+           
+        return n-999; 
     }
 }

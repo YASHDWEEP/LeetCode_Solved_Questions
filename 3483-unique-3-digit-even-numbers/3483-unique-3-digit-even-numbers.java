41ef0cbfe6dcca3362fb9de74eee10b8dbe6344d
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-11 13:05:25 +0530

    Time: 1 ms (100%), Space: 44 MB (88.67%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,32 +1,33 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        int ans = 0;
-        int freq[] = new int[10];
-        for (int i = 0; i < digits.length; i++) {
-            freq[digits[i]]++;
+        int[] freq = new int[10];
+        for(int digit:digits) freq[digit]++;
+        int zero=0, even=0, all=0;
+
+        for(int i=0; i<10; i++)if (freq[i]>0){
+            if (i==0) zero++;
+            if (i%2==0) even++;
+            all++;
         }
-        for (int num = 100; num <= 998; num++) {
-            if (num % 2 != 0) {
-                continue;
-            }
-            int used[] = new int[10];
-            int n = num;
-            used[n % 10]++;
-            n = n / 10;
-            used[n % 10]++;
-            n = n / 10;
-            used[n % 10]++;
-            boolean possible = true;
-            for (int d = 0; d <= 9; d++) {
-                if (used[d] > freq[d]) {
-                    possible = false;
-                    break;
-                }
-            }
-            if (possible) {
-                ans++;
+        
+
+        int count = even*(all-1)*(all-2);
+        if (zero==1) count-=(even-1)*(all-2);
+
+        for(int i=0; i<10; i++){
+            if (freq[i]>=2){
+                if (i==0) count+=all-1;
+                else if (i%2==1) count+=even;
+                else{
+                    count+=3*(even-1)-zero;
+                    count+=2*(all-even);
+                }           
             }
         }
-        return ans;
+
+        for(int i=2; i<10; i+=2)if (freq[i]>=3)count++;
+            
+        
+        return count;
     }
 }

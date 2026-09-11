17029b681793752907a96822aee1b32066d2a921
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-11 16:28:11 +0530

    Time: 1 ms (100%), Space: 44.1 MB (29.35%) - LeetHub

---
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] evenOddBit(int n) {
+        String binary = Integer.toBinaryString(n);
+       
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if(binary.charAt(i)== '1'){
+                int bitposition = binary.length() -1 - i; 
+                if (bitposition % 2 == 0){
+                    even++;
+                }else {
+                    odd++;
+                }
+            }
+         }
+        return new int[] {  even ,odd};
+    }
+}

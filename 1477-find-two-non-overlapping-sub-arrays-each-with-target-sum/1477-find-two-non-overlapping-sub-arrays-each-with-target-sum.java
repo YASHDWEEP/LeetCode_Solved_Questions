71ead5e98e062f8bafe787afaa6d076bb6680941
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-17 12:47:04 +0530

    Time: 7 ms (84.3%), Space: 91.1 MB (50.67%) - LeetHub

---
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        int best[] = new int[n];
+        int left = 0;
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+        Arrays.fill(best, Integer.MAX_VALUE);
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+            while (sum > target) {
+                sum -= arr[left];
+                left++;
+            }
+            if (sum == target) {
+                int len = right - left + 1;
+                if (left > 0 && best[left - 1] != Integer.MAX_VALUE) {
+                    ans = Math.min(ans, best[left - 1] + len);
+                }
+                if (right == 0) {
+                    best[right] = len;
+
+                } else {
+                    best[right] = Math.min(best[right - 1], len);
+                }
+            } else {
+                if (right > 0) {
+                    best[right] = best[right - 1];
+                }
+            }
+
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}

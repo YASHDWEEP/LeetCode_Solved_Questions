d8ac0b50348ce1c4849791eba3351956f99cee15
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-17 12:47:29 +0530

    Time: 5 ms (100%), Space: 90.7 MB (88.34%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,35 +1,31 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
         int n = arr.length;
-        int best[] = new int[n];
-        int left = 0;
-        int sum = 0;
+        int[] minLen = new int[n];
+        int l = 0, sum = 0;
         int ans = Integer.MAX_VALUE;
-        Arrays.fill(best, Integer.MAX_VALUE);
-        for (int right = 0; right < n; right++) {
-            sum += arr[right];
+        int best = Integer.MAX_VALUE;
+        
+        for (int r = 0; r < n; r++) {
+            sum += arr[r];
+            
             while (sum > target) {
-                sum -= arr[left];
-                left++;
+                sum -= arr[l++];
             }
+            
             if (sum == target) {
-                int len = right - left + 1;
-                if (left > 0 && best[left - 1] != Integer.MAX_VALUE) {
-                    ans = Math.min(ans, best[left - 1] + len);
+                int currLen = r - l + 1;
+                
+                if (l > 0 && minLen[l - 1] != Integer.MAX_VALUE) {
+                    ans = Math.min(ans, currLen + minLen[l - 1]);
                 }
-                if (right == 0) {
-                    best[right] = len;
-
-                } else {
-                    best[right] = Math.min(best[right - 1], len);
-                }
-            } else {
-                if (right > 0) {
-                    best[right] = best[right - 1];
-                }
+                
+                best = Math.min(best, currLen);
             }
-
+            
+            minLen[r] = best;
         }
+        
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }

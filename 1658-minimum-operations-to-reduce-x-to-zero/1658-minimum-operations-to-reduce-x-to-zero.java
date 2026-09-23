92d33bfa3342ddf5f4dd7166abf7a006ac091965
YASHDWEEP <rewatkhedkar@gmail.com>
2026-09-23 20:17:00 +0530

    Time: 4 ms (98.07%), Space: 102 MB (76.11%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,27 +1,30 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int totalsum = 0;
+        int total_sum = 0;
         for (int n : nums) {
-            totalsum += n;
+            total_sum += n;
         }
-        int target = totalsum - x;
+        int target = total_sum - x;
         if (target < 0) {
             return -1;
         }
         int left = 0;
         int current_sum = 0;
-        int longest_subarray_length = 0;
+        int longest_length = 0;
+
         boolean isfound = false;
         for (int right = 0; right < nums.length; right++) {
             current_sum += nums[right];
             while (current_sum > target) {
                 current_sum -= nums[left++];
+
             }
             if (current_sum == target) {
-                longest_subarray_length = Math.max(longest_subarray_length, right - left + 1);
+                longest_length = Math.max(longest_length, right - left + 1);
                 isfound = true;
             }
+
         }
-        return isfound ? nums.length - longest_subarray_length : -1;
+        return isfound ? nums.length - longest_length : -1;
     }
 }
